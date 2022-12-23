d905a0e9466ee6a3e806da5e63c58d8a775f50aa
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-23 17:51:37 +0600

    chat stripe

--- script.js
+++ script.js
@@ -1,39 +1,55 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
+import bot from "./assets/bot.svg";
+import user from "./assets/user.svg";
 
-const form = document.querySelector('form');
-const chatContainer = document.querySelector('#chat_container');
+const form = document.querySelector("form");
+const chatContainer = document.querySelector("#chat_container");
 
 let loadInterval;
 
 function loader(element) {
-  element.textContent = '';
+  element.textContent = "";
 
   loadInterval = setInterval(() => {
-    element.textContent += '.';
+    element.textContent += ".";
 
-    if(element.textContent === '....'){
-      element.textContent = '';
+    if (element.textContent === "....") {
+      element.textContent = "";
     }
-  },300)
+  }, 300);
 }
 
 function typeText(element, text) {
   let index = 0;
   let interval = setInterval(() => {
-    if(index < text.length){
+    if (index < text.length) {
       element.innerHTML += text.chartAt(index);
-      index ++;
-    }else{
+      index++;
+    } else {
       clearInterval(interval);
     }
-  }, 20)
+  }, 20);
 }
 
-function generateUniqueId(){
+function generateUniqueId() {
   const timeStamp = Date.now();
   const randomNumber = Math.random();
   const hexadecimalString = randomNumber.toString(16);
 
   return `id-${timeStamp}-${hexadecimalString}`;
+}
+
+function chatStripe(isAI, value, uniqueId) {
+  return `
+  <div class="wrapper ${isAI && "ai"}">
+    <div class="chat">
+      <div class="profile">
+        <img 
+        src="${isAI ? bot : user}"
+        alt="${isAI ? 'bot' : 'user'}"
+        />
+      </div>
+      <div class="message" id="${uniqueId}">${value}</div>
+    </div>
+  </div>
+  `;
 }
