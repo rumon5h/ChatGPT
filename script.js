487350e084500590e5049b32a6690f75ce1bd046
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-23 21:00:07 +0600

    done

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ function typeText(element, text) {
   let index = 0;
   let interval = setInterval(() => {
     if (index < text.length) {
-      element.innerHTML += text.chartAt(index);
+      element.innerHTML += text.charAt(index);
       index++;
     } else {
       clearInterval(interval);
@@ -38,14 +38,14 @@ function generateUniqueId() {
   return `id-${timeStamp}-${hexadecimalString}`;
 }
 
-function chatStripe(isAI, value, uniqueId) {
+function chatStripe(isAi, value, uniqueId) {
   return `
-  <div class="wrapper ${isAI && "ai"}">
+  <div class="wrapper ${isAi && "ai"}">
     <div class="chat">
       <div class="profile">
         <img 
-        src="${isAI ? bot : user}"
-        alt="${isAI ? 'bot' : 'user'}"
+        src="${isAi ? bot : user}"
+        alt="${isAi ? "bot" : "user"}"
         />
       </div>
       <div class="message" id="${uniqueId}">${value}</div>
@@ -60,25 +60,49 @@ const handleSubmit = async (event) => {
   const data = new FormData(form);
 
   // user's chatStripe
-  chatContainer.innerHTML += chatStripe(false, data.get('prompt'));
+  chatContainer.innerHTML += chatStripe(false, data.get("prompt"));
 
   form.reset();
 
   // bot's chatStripe
   const uniqueId = generateUniqueId();
-  chatContainer.innerHTML += chatStripe(true, " ", uniqueId); 
+  chatContainer.innerHTML += chatStripe(true, " ", uniqueId);
 
   chatContainer.scrollTop = chatContainer.scrollHeight;
 
   const messageDiv = document.getElementById(uniqueId);
 
-  loader(messageDiv)
+  loader(messageDiv);
 
-}
+  // fetch data from server -> bot's response
+  const response = await fetch("https://chatgpt-s17q.onrender.com/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      prompt: data.get("prompt"),
+    }),
+  });
 
-form.addEventListener('submit', handleSubmit)
-form.addEventListener('keyup', (event) => {
-  if(event.keyCode === 13) {
+  clearInterval(loadInterval);
+
+  messageDiv.innerHTML = "";
+  if (response.ok) {
+    const data = await response.json();
+    const parsedData = data.bot.trim();
+
+    typeText(messageDiv, parsedData);
+  } else {
+    const err = response.text();
+    messageDiv.innerHTML = err;
+    alert(err);
+  }
+};
+
+form.addEventListener("submit", handleSubmit);
+form.addEventListener("keyup", (event) => {
+  if (event.keyCode === 13) {
     handleSubmit(event);
   }
-})
+});
