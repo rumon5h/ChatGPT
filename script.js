bb86040001c1a441bfbdef061587b6faca524aed
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-23 17:42:09 +0600

    random id

--- script.js
+++ script.js
@@ -29,3 +29,11 @@ function typeText(element, text) {
     }
   }, 20)
 }
+
+function generateUniqueId(){
+  const timeStamp = Date.now();
+  const randomNumber = Math.random();
+  const hexadecimalString = randomNumber.toString(16);
+
+  return `id-${timeStamp}-${hexadecimalString}`;
+}
