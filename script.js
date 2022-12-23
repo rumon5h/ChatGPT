f70e88a2380f0b6d272128fa143fe15b7c1c050f
rumon5h <mdrumonkhan48519@gmail.com>
2022-12-23 17:33:17 +0600

    loader script

---
+++ script.js
@@ -0,0 +1,19 @@
+import bot from './assets/bot.svg';
+import user from './assets/user.svg';
+
+const form = document.querySelector('form');
+const chatContainer = document.querySelector('#chat_container');
+
+let loadInterval;
+
+function loader(element) {
+  element.textContent = '';
+
+  loadInterval = setInterval(() => {
+    element.textContent += '.';
+    
+    if(element.textContent === '....'){
+      element.textContent = '';
+    }
+  },300)
+}
